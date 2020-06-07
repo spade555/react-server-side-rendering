923628db21f55c9be92e3506057d69ca802efd55
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2020-06-07 20:37:26 +0200

    Simple express server created with a start script.

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = new express();
+
+app.get('/', async (_req, res) => {
+  res.send(
+    `<h2>Hello World!</h2>`
+  )
+});
+
+app.listen(5555);
+console.log('Server is listening on 5555');
