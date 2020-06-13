c9611eee4e183b8d23742603edf0b83d04bd94e5
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2020-06-13 16:31:44 +0200

    Add some dummy data to the server to represent the application state and pass them to the App component through props.

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
 
-export const App = () => (
-  <div>This is a React component.</div>
+export const App = ({questions, answers}) => (
+  <div>
+    <h1>
+      Q&A Tool
+    </h1>
+    {questions.map(({questionId, content}) => (
+      <div key={questionId}>
+        <h3>
+          {content}
+        </h3>
+        <div>
+          {answers.filter(answer => answer.questionId === questionId).map(
+            ({content, upvotes, answerId})=>
+              <div key={answerId}>
+                <span>
+                  {content} - {upvotes}
+                </span>
+              </div>
+          )}
+        </div>
+      </div>
+    ))}
+  </div>
 );
--- server/index.js
+++ server/index.js
@@ -6,13 +6,54 @@ import { renderToString } from 'react-dom/server';
 
 import { App } from './../client/App';
 
+const data = {
+    questions:[{
+        questionId:"Q1",
+        content:"Which backend solution should we use for our application?"
+    },{
+        questionId:"Q2",
+        content:"What percentage of developer time should be devoted to end-to-end testing?"
+    }],
+    answers:[{
+        answerId:"A1",
+        questionId:"Q1",
+        upvotes:2,
+        content: "Apache"
+    },{
+        answerId:"A2",
+        questionId:"Q1",
+        upvotes:0,
+        content:"Java"
+    },{
+        answerId:"A3",
+        questionId:"Q1",
+        upvotes:4,
+        content:"Node.js"
+    },{
+        answerId:"A4",
+        questionId:"Q2",
+        upvotes:2,
+        content:"25%"
+    },{
+        answerId:"A5",
+        questionId:"Q2",
+        upvotes:1,
+        content:"50%"
+    },{
+        answerId:"A6",
+        questionId:"Q2",
+        upvotes:1,
+        content:"75%"
+    }]
+};
+
 const app = new express();
 
 app.use(express.static('dist'));
 
 app.get('/', async (_req, res) => {
   const index = readFileSync('public/index.html', 'utf8');
-  const rendered = renderToString(<App/>);
+  const rendered = renderToString(<App {...data} />);
   res.send(index.replace("{{rendered}}", "Rendered on Server: " + rendered));
 });
 
