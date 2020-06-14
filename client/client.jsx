42b9ad6d503004c7c1c98b6e3d1e33df8421a5ad
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2020-06-14 20:17:40 +0200

    Client code modified to fetch the data from the server and hydrate the App comp after data is received.

--- client/client.jsx
+++ client/client.jsx
@@ -2,4 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
-ReactDOM.render(<App/>, document.querySelector("#Container"));
+let state = undefined;
+
+fetch("http://localhost:5555/data")
+  .then(data => data.json())
+  .then(json => {
+    state = json;
+    console.log("Got the state.");
+    render();
+  });
+
+const render = () => {
+  ReactDOM.hydrate(<App {...state} />, document.querySelector("#Container"));
+}
